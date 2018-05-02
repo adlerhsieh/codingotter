6fb397fa14bff721acae0ca15d69ef1146551631
Adler <nkj20932@hotmail.com>
2018-05-02 22:30:42 +1000

    render markdown from BE

--- Gemfile
+++ Gemfile
@@ -38,6 +38,9 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 gem 'dotenv-rails'
 gem 'kaminari'
+gem 'friendly_id'
+gem 'coderay'
+gem 'redcarpet'
 
 group :development, :test do
   gem 'rspec-rails'
--- app/models/screencast.rb
+++ app/models/screencast.rb
@@ -1,2 +1,14 @@
 class Screencast < ApplicationRecord
+
+  extend FriendlyId
+
+  friendly_id :slug
+
+  def render_content
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, 
+                                        fenced_code_blocks: true
+                                       )
+    markdown.render(content).html_safe
+  end
+
 end
